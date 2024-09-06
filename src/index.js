597f83cfdd849d7022840c5756225718f6d361f5
Laxmikantadas123 <daslaxmikanta711@gmail.com>
2024-09-06 11:29:51 +0530

    work on util

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
+const port=process.env.PORT||3000
 dotenv.config(); 
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listion(()=>{
+        console.log(`server is running in port ${port}`)})
+    })
+   
+.catch((e)=>{
+console.log(`database is not connected ${e}`)
+})
