5ca0b1c619b33acf58aec432005d8042c59de764
Laxmikantadas123 <daslaxmikanta711@gmail.com>
2024-09-10 11:01:55 +0530

    add register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import { asyncHandler} from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 const registerUser = asyncHandler(async (req, res) => {
@@ -36,7 +37,14 @@ const registerUser = asyncHandler(async (req, res) => {
       password,
       username:username.toLowerCase()
     })
-    const createUser=await User.findById(user._id).se
+    const createUser=await User.findById(user._id).select("-password -refreshToken")
+
+    if(!createUser){
+      throw new ApiError(500,"something went wrong while registering the user")
+    }
+    return res.status(201).json(
+      new ApiResponse(200,createUser,"User Registered Successfully")
+    )
 });
 
 export { registerUser };
